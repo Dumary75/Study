591a9bc552f9a4330113b5b81930a527f350aee6
Dumary75 <giesal.otto@web.de>
2025-02-19 20:38:50 +0100

    Movie_List Almost finish! Just the Imagebug is open...

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from "react"
-import Chart from './components/Chart.jsx';
-import Creater from './components/Creater.jsx';
+import { useEffect, useState } from "react";
+import Card from './components/Card.jsx';
 
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTZmMzUwMWM0MmY4ODc1MGM0ZmJmYThkZDMzMmM4ZCIsIm5iZiI6MTczOTM4MTM5OS44MSwic3ViIjoiNjdhY2RhOTcxMTY4ZGVkYTEwOWYzMDc3Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.4wUSyZdxq7cK1Pp7i3HMGKd1pJFcPSoHVM7NbCvv4NA'
+  }
+};
 
 export default function App() {
-  const[mainArray,setMainArray] = useState([]);
+  const[movieList,setList] = useState([])
+
+ async function dataFetch() {
+  try {
+    let data = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options);
+    let dataConvert = await data.json();
+    setList(dataConvert.results);
+  } catch (error) {
+    console.log('FEHLER: ' + error);
+  }}
+
+  useEffect(() => {dataFetch()}, []);
+
 
   return (
       <>
-       <Creater mainArray={mainArray} setMainArray={setMainArray} />
-       {mainArray.length ? <Chart mainArray={mainArray}/> : 'Found no Charts, create some!'} 
+      <div className="cardContainer">
+        {
+          movieList.map((movie,index) => (
+              <Card key={index} img={"https://picsum.photos/200/300"} title={movie.title} text={movie.overview}/>
+          ))
+        }
+      </div>
       </>
     )
 };
