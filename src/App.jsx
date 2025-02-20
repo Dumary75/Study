5a90a2b204fdbd58139d549f0b82fbe21783dc7f
Dumary75 <giesal.otto@web.de>
2025-02-20 11:04:38 +0100

    Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import { useEffect, useState } from "react";
-import Card from './components/Card.jsx';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: '#'
-  }
-};
-
-export default function App() {
-  const[movieList,setList] = useState([])
-
- async function dataFetch() {
-    let data = await fetch('https://api.themoviedb.org/3/discover/movie?inlude_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options);
-    let dataConvert = await data.json();
-    setList(dataConvert.results);
-  }
-
-  useEffect(() => {dataFetch()}, []);
-
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-      <>
-      <div className="cardContainer">
-        {
-          movieList ? 
-             movieList.map((movie,index) => (
-            <Card key={index} img={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} title={movie.title} text={movie.overview}/>
-        )) : <h3>Please Update the API_KEY in "Authorization"</h3>
-        }
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      </>
-    )
-};
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
+export default App
