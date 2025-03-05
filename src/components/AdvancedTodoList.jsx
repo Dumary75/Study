810e27f55de3bc980c6350dfb0cd2852fc8b49a5
Dumary75 <gleisal.neu@proton.me>
2025-03-05 12:03:53 +0100

    Added a filter function

--- src/components/AdvancedTodoList.jsx
+++ src/components/AdvancedTodoList.jsx
@@ -5,6 +5,7 @@ import { FilterList } from "./FilterList";
 export function TodoList(){
 const[todoList,setTodoList] = useState([]);
 const[newList,setNewList] = useState({});
+const[filteredList,setFilteredlist] = useState([]);
 
 const inputChanger = (event) => {
     setNewList({ 
@@ -33,7 +34,7 @@ const changeEditmode = (index) => {
     setTodoList(changedTaskList);
 }
 
-const savePressed = (index,event) => {
+const savePressed = (index) => {
     let ChangedTodoName = todoList.map((element, i) =>
         i === index ? { ...element, 
             editMode: !element.editMode ? true : false,
@@ -49,7 +50,7 @@ const savePressed = (index,event) => {
     return(
         <>
          {/*  Elements above the list */}
-        <FilterList todoList={todoList}/>
+        <FilterList setFilteredlist={setFilteredlist} todoList={todoList}/>
         <CreateTodo todoList={todoList} 
         setTodoList={setTodoList} 
         newList={newList} 
@@ -57,26 +58,76 @@ const savePressed = (index,event) => {
         inputChanger={inputChanger}/>
         
 
-            <div>
-                {todoList.length > 0 ? 
+            
+            { /* If FILTERED*/ filteredList.length > 0 ? (
                 <ul>
-                {todoList.map((listitem,index) => {
-                   return  <div className={listitem.state === 'active' ? "Task active" : "Task complete"} id={index} key={index}>
-                        {!listitem.editMode ? 
-                        <>
-                        <li>{listitem.task}</li> 
-                        <input onChange={() => changeTaskState(index)} type="checkbox" />
-                        <button onClick={() => removeTask(index)} className="remove_Task">❌</button>
-                        <button onClick={() => changeEditmode(index)} className="EditBotton">Edit</button>
-                        </>
-                        : <>
-                        <input placeholder={listitem.task} className="EditTodoName" onChange={inputChanger}></input>
-                        <button onClick={(event) => savePressed(index, event)} className="EditBotton">Save</button>
-                        </> }        
-                    </div>
-                })}
-                </ul> : <h3>No ListItems aviable, create some!</h3>}
+                    {filteredList.map((listitem, index) => {
+      return (
+        <div
+          className={listitem.state === 'active' ? 'Task active' : 'Task complete'}
+          id={index}
+          key={index}
+        >
+          {!listitem.editMode ? (
+                <>
+                   <li>{listitem.task}</li>
+                          <input onChange={() => changeTaskState(index)} type="checkbox" />
+                          <button onClick={() => removeTask(index)} className="remove_Task">❌</button>
+                          <button onClick={() => changeEditmode(index)} className="EditBotton">Edit</button>
+                </>
+            ) : (
+                <>
+                <input
+                    placeholder={listitem.task}
+                    className="EditTodoName"
+                    onChange={inputChanger}
+                />
+                <button onClick={(event) => savePressed(index, event)} className="EditBotton">
+                    Save
+                </button>
+                </>
+            )}
             </div>
+        );
+        })}
+                </ul>
+            ) : /* If NOT filtered*/todoList.length > 0 ? (
+                <ul>
+                  {todoList.map((listitem, index) => {
+      return (
+        <div
+          className={listitem.state === 'active' ? 'Task active' : 'Task complete'}
+          id={index}
+          key={index}
+        >
+          {!listitem.editMode ? (
+                <>
+                   <li>{listitem.task}</li>
+                          <input onChange={() => changeTaskState(index)} type="checkbox" />
+                          <button onClick={() => removeTask(index)} className="remove_Task">❌</button>
+                          <button onClick={() => changeEditmode(index)} className="EditBotton">Edit</button>
+                </>
+            ) : (
+                <>
+                <input
+                    placeholder={listitem.task}
+                    className="EditTodoName"
+                    onChange={inputChanger}
+                />
+                <button onClick={(event) => savePressed(index, event)} className="EditBotton">
+                    Save
+                </button>
+                </>
+            )}
+            </div>
+        );
+        })}
+    </ul>
+    ) : ( /* If NO Todos created */
+    <h3>No ListItems available, create some!</h3>
+    )}
+
+          
         </>
     )
 }
--- src/components/FilterList.jsx
+++ src/components/FilterList.jsx
@@ -1,21 +1,37 @@
 import { useState } from "react"
 
-export function FilterList({ todoList }){
+export function FilterList({ setFilteredlist, todoList }){
 const[listMode,setlistMode] = useState(false);
 
 const chanceListMode = () => {
     setlistMode((prevState) => !prevState)
 }
 
+const activeTaskFilter = () => {
+    const activeList = todoList.filter((element) => element.state === 'active');
+    setFilteredlist(activeList);
+};
+
+const completeTaskFilter = () => {
+    const completedList = todoList.filter((element) => element.state === 'complete');
+    setFilteredlist(completedList);
+};
+
+const viewAllFilter = () => {
+    setFilteredlist([])
+};
+
+
     return (
         <>
         {listMode === true? 
                 <ul className="FilterListUL">
-                <li><button>Active Tasks</button></li>
-                <li><button>Completed Tasks</button></li>
-                <li><button>View All</button></li>
-                <li><button onClick={chanceListMode}>Close</button></li>
-            </ul> : <button onClick={chanceListMode} className="FilterListButton">List</button>}
+                    <li><button onClick={activeTaskFilter}>Active Tasks</button></li>
+                    <li><button onClick={completeTaskFilter}>Completed Tasks</button></li>
+                    <li><button onClick={viewAllFilter}>View All</button></li>
+                    <li><button onClick={chanceListMode}>Close</button></li>
+                </ul> : 
+                        <button onClick={chanceListMode} className="FilterListButton">List</button>}
 
         
         </>
