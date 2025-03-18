63c404f2b9aa5762dc705b5ebb447e54618d3b67
Dumary75 <gleisal.neu@proton.me>
2025-03-18 18:40:41 +0100

    Started to learn form validation, to check the password strengh etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,57 @@
-
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
+import { useState } from 'react';
+import validator from 'validator';
 
 
 function App() {
+const[user,setUser] = useState({
+  username: '',
+  mail: '',
+  password: '',
+  confirmPassword: ''
+})
 
-  const locationApp = useLocation();
+const[mailValidator,setMailValidator] = useState(false);
 
+const emailRegEx = /^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+const mailChecker = () => {
+  if(emailRegEx.test(user.mail)){
+      setMailValidator(true);
+  } else {setMailValidator(false)}
+}
 
+const mailChanger = (event) => {
+ let mailValue = event.target.value.toLowerCase()
 
-useEffect(() => {    
-  if(locationApp.state.from === 'FirstPage'){
-  alert('JA')
-}},[])
+  setUser(
+    {...user,mail: mailValue}
+  )
+
+  mailChecker();
+}
+
+const userNameChanger = (event) => {
+  let userValue = event.target.value.toLowerCase()
+
+  setUser({...user,
+    username: userValue
+  })
+
+}
 
   return (
     <>
-    <h3>APP, u came from {locationApp.state.from}</h3>
+    
+    <div className="formBlock">
+      <form>
+        <label htmlFor="userName">Username:</label>
+        <input type="text" name='userName' className={user.username.length>0 ? 'validate' : 'invalide'} placeholder='Username' onChange={userNameChanger}/>
+
+        <label htmlFor="mail">Email:</label>
+        <input type="text" name='mail' className={mailValidator ? 'validate' : 'invalide'} onChange={mailChanger} placeholder='Email'/>
+      </form>
+    </div>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Users from './Users.jsx'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-         
-         <Route path='/users' element={<Users />} />
-          <Route path='app' element={<App />} />
-
-
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
