44fa8dbdd1100497e6ea747ba70f6b909dd16db9
Dumary75 <gleisal.neu@proton.me>
2025-03-25 14:10:10 +0100

    Fresh-Restart to learn new things about debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
-import { useState } from 'react'
-import Header from './components/Header.jsx'
-import ResaultTable from './components/ResualtTable.jsx';
+
 
 function App() {
-const[investmentData,setInvestData] = useState({
-  initialInvestment: 100,
-  annualInvestment: 0,
-  expectedReturn: 0,
-  duration: 0
-})
-
-const[initialInvestment,setInitialInvest] = useState(0);
-const[annualInvestment,setAnnualinvest] = useState(0);
-const[expectedReturn,setExpReturn] = useState(0);
-const[duration,setDuration] = useState(0);
 
 
 
 
   return (
     <>
-    <Header investmentData={investmentData} setInvestData={setInvestData}/>
-    <ResaultTable InvestMentData={investmentData} />
+
     </>
 
   )
