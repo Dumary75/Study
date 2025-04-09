ecf33ec838f5d48cb09519046b82fd2451213b71
Dumary75 <gleisal.neu@proton.me>
2025-04-09 15:37:44 +0200

    Started a new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import Player from './components/Player.jsx';
-import Challenge from './components/Challenge.jsx';
+
 
 function App() {
   return (
     <>
-      <Player />
-      <div id="challenges">
-          <Challenge titel={'Easy'} timelimit={1}/>
-          <Challenge titel={'Medium'} timelimit={5}/>
-          <Challenge titel={'Pro'} timelimit={10}/>
-          <Challenge titel={'Maximum'} timelimit={15}/>
-      </div>
+
     </>
   );
 }
