190a4505ceee659bc6e1d015ed92e00125d628b1
Dumary75 <gleisal.neu@proton.me>
2025-04-18 12:29:25 +0200

    Added the first sideEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import sortPlacesByDistance from './loc.js';
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+  const[availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState([]);
+
+
+ useEffect(() => {
+    
+    navigator.geolocation.getCurrentPosition((position) => {
+        const sortedPlaces = sortPlacesByDistance(
+            AVAILABLE_PLACES,
+            position.coords.latitude,
+            position.coords.longitude
+        );
+
+        setAvailablePlaces(sortedPlaces);
+    });
+
+ },[]) ;
+
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,6 +46,11 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if(storedIds.indexOf(id) === -1){
+        localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
+    }
   }
 
   function handleRemovePlace() {
@@ -63,7 +86,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          fallbackText={'Sorting places by distance...'}
+          places={availablePlaces}
           onSelectPlace={handleSelectPlace}
         />
       </main>
