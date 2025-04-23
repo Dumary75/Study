3b30ab1e72d07c68ac08374a4a30feeb867dd087
Dumary75 <gleisal.neu@proton.me>
2025-04-23 11:05:14 +0200

    Stardet to learn about React19 - form_Actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import Header from './components/Header.jsx';
+import Signup from './components/Signup.jsx';
+
 function App() {
   return (
     <>
-      <h2>TEST</h2>
+      <Header />
+      <main>
+        <Signup />
+      </main>
     </>
   );
 }
+
 export default App;
