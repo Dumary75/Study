fa63ca6cdf2f5724caa192d70157fa5b35f1e253
Dumary75 <gleisal.neu@proton.me>
2025-05-06 17:48:21 +0200

    Just tested the lazyloading function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { lazy, Suspense, useState } from 'react';
 
+const TestLazy = lazy(() => import('./components/Test.jsx'));
 
 function App() {
+const[lazy,Setlazy] = useState(false);
+
+const setter = () => {
+  Setlazy((oldState) => !oldState)
+}
 
   return (
     <>
+      <button onClick={setter}>X</button>
+
+      {lazy? 
+            <Suspense fallback='Uff... das Dauert!'>
+            <TestLazy />
+        </Suspense> : 'Nix'  
+    }
 
     </>
   )
