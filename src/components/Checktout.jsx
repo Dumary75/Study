71db2425847a6e7e5563839fcf9932bfadf66169
Dumary75 <gleisal.neu@proton.me>
2025-05-27 14:31:44 +0200

    Added a post function, so u can now send a order to the backend, which will be saved in the "orders" file

--- src/components/Checktout.jsx
+++ src/components/Checktout.jsx
@@ -20,12 +20,33 @@ const userProgressCtx = useContext(UserProgressContext);
         userProgressCtx.hideCart();
     };
 
+    function handleSubmit(event){
+        event.preventDefault();
+
+        const fd = new FormData(event.target);
+        const customerData = Object.fromEntries(fd.entries());
+
+        fetch('http://localhost:3000/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                order: {
+                    items: cartCtx.items,
+                    customer: customerData
+                }
+              })
+            });
+          }
+
+
     return <Modal open={userProgressCtx.progress === 'checkout'}>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>Checkout</h2>
             <p>Total Amount: {cartTotal} $</p>
 
-            <Input label='Full Name' type='text' id='full-name'/>
+            <Input label='Full Name' type='text' id='name'/>
             <Input label='E-Mail Adress' type='email' id='email'/>
             <Input label='Street' type='text' id='street'/>
             <div className="control-row">
@@ -35,7 +56,7 @@ const userProgressCtx = useContext(UserProgressContext);
 
             <p className="modal-actions">
                 <Button textOnly type='button' onClick={handleCloseModal}>Close</Button>
-                <Button textOnly type='button' onClick={handleCloseModal}>Submit Order</Button>
+                <Button textOnly onClick={handleCloseModal}>Submit Order</Button>
             </p>
         </form>
     </Modal>
