d94326ed2b2c006b17d085c4f271bde2f764e93e
Dumary75 <gleisal.neu@proton.me>
2025-05-27 15:24:55 +0200

    Time for a new project, to learn about redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import Header from './components/Header.jsx';
-import Products from './components/Products.jsx';
-import { CartContextProvider } from './components/FunctionComponents/CartContext.jsx';
-import { UserProgressContextProvider } from './components/FunctionComponents/UserProgressContext.jsx';
-import Cart from './components/Cart.jsx';
-import Checkout from './components/Checktout.jsx';
+
 
 function App() {
 
   return (
-    <UserProgressContextProvider>
-      <CartContextProvider>
-          <Header />
-          <Products />
-          <Cart />
-          <Checkout />
-      </CartContextProvider>
-    </UserProgressContextProvider>
+    <>
+    
+    </>
   );
 }
 
