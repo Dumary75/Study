5ab0db9db8f32024f0e7219191ae745d676e8401
Dumary75 <gleisal.neu@proton.me>
2025-06-12 15:01:52 +0200

    Started to learn about "Tanstack Query"

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('test und so', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/TEST/i);
-  expect(linkElement).toBeInTheDocument();
-});
