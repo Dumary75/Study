097c30172d3a0cd31f283d626900e9afdc8a4a26
Dumary75 <gleisal.neu@proton.me>
2025-07-08 14:28:54 +0200

    Task solved, cleared everything for a new one

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 
-import UserList from './components/UserList.jsx';
-import { UserProvider } from './components/UserContext.jsx';
-import { ModalProvider } from './components/ModalContext.jsx';
+
 
 function App() {
 
 
   return (
-    <ModalProvider>
-      <UserProvider>
-        <UserList />
-      </UserProvider>
-    </ModalProvider>
+    <>
+    
+    
+    </>
   )
 }
 
