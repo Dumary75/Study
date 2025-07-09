8954af808cbf42aec82c4c5a7b73c50a55c97740
Dumary75 <gleisal.neu@proton.me>
2025-07-09 12:11:19 +0200

    Working with tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
+
+const queryClient = new QueryClient();
 
 function App() {
-
-
   return (
     <>
-    
-    
+        <QueryClientProvider client={queryClient}>
+            <Todos />
+            <AddTodo />
+        </QueryClientProvider>
     </>
-  )
+  );
 }
 
 export default App
