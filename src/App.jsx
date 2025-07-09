28bf3d9114b8280c3283102cc80c93a81cf692c9
Dumary75 <gleisal.neu@proton.me>
2025-07-09 16:41:31 +0200

    Started to learn about next.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
 
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-        <QueryClientProvider client={queryClient}>
-            <Todos />
-            <AddTodo />
-        </QueryClientProvider>
+
     </>
   );
 }
