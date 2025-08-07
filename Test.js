d78634a809df33c6edee644b281ef725a3774d1d
Dumary75 <gleisal.neu@proton.me>
2025-08-07 15:32:59 +0200

    Finished ai challenge

--- Test.js
+++ Test.js
@@ -8,7 +8,6 @@ var BookCategory;
 ;
 ;
 ;
-var books = [];
 var potter = {
     title: 'potter',
     author: 'marks',
@@ -16,7 +15,19 @@ var potter = {
     category: BookCategory.Fiction,
     available: true
 };
-books.push(potter);
+var ww2 = {
+    title: 'ww2',
+    author: 'historac',
+    isbn: '1945',
+    category: BookCategory.History,
+    available: true
+};
+var books = [potter, ww2];
+var Eddy = {
+    name: 'Eddy',
+    userId: 75,
+    borrowedBooks: []
+};
 function findItemByTitle(Title, books) {
     var foundBook = books.find(function (book) { return book.title === Title; });
     if (foundBook) {
@@ -27,6 +38,77 @@ function findItemByTitle(Title, books) {
         console.log('Kein Treffer!');
         return null;
     }
+    ;
 }
-findItemByTitle('test', books);
-findItemByTitle('potter', books);
+;
+var Library = /** @class */ (function () {
+    function Library() {
+    }
+    Library.prototype.addBook = function (book) {
+        books.push(book);
+    };
+    ;
+    Library.prototype.getAvailableBooks = function () {
+        if (books.length > 0) {
+            books.forEach(function (book) { return console.log(book.title); });
+        }
+        else {
+            console.log('No books found!');
+        }
+        ;
+    };
+    ;
+    Library.prototype.borrowBook = function (isbn, user) {
+        var foundBook = books.find(function (book) { return book.isbn === isbn; });
+        if (foundBook) {
+            if ((foundBook === null || foundBook === void 0 ? void 0 : foundBook.available) === true) {
+                user.borrowedBooks.push(foundBook);
+                foundBook.available = false;
+                console.log("".concat(foundBook.title, " wurde an ").concat(user.name, " ausgeliehen!"));
+            }
+            else {
+                console.log("".concat(foundBook.title, " ist nicht verf\u00FCgbar!"));
+            }
+        }
+        else {
+            console.log('Buch wurde nicht gefunden!');
+        }
+    };
+    ;
+    Library.prototype.returnBook = function (isbn, user) {
+        var foundBook = books.find(function (book) { return book.isbn === isbn; });
+        if (foundBook) {
+            if (foundBook.available === false) {
+                user.borrowedBooks = user.borrowedBooks.filter(function (foundBook) { return !foundBook; });
+                foundBook.available = true;
+                console.log("".concat(foundBook.title, " wurde von ").concat(user.name, " zur\u00FCckgegeben!"));
+            }
+            else {
+                console.log("".concat(foundBook.title, " wurde bereits zur\u00FCckgegeben!"));
+            }
+            ;
+        }
+        else {
+            console.log('Buch wurde nicht gefunden!');
+        }
+    };
+    ;
+    Library.prototype.getBooksByCategory = function (category, books) {
+        var foundBook = books.find(function (book) { return book.category === category; });
+        if (foundBook) {
+            console.log("Treffer: ".concat(foundBook.category, "!"));
+            return foundBook;
+        }
+        else {
+            console.log('Kein Treffer!');
+            return null;
+        }
+    };
+    return Library;
+}());
+;
+var BookChanger = new Library;
+BookChanger.borrowBook('1945', Eddy);
+BookChanger.returnBook('1945', Eddy);
+BookChanger.getBooksByCategory(BookCategory.Fiction, books);
+BookChanger.returnBook('1945', Eddy);
--- Test.tsx
+++ Test.tsx
@@ -22,8 +22,6 @@ interface User {
     borrowedBooks: Book[]
 };
 
-let books: Book[] = [];
-
 const potter: Book = {
     title: 'potter',
     author: 'marks',
@@ -32,7 +30,22 @@ const potter: Book = {
     available: true
 };
 
-books.push(potter);
+const ww2: Book = {
+  title: 'ww2',
+  author: 'historac',
+  isbn: '1945',
+  category: BookCategory.History,
+  available: true
+};
+
+let books: Book[] = [potter,ww2];
+
+const Eddy: User = {
+  name: 'Eddy',
+  userId: 75,
+  borrowedBooks: []
+};
+
 
 function findItemByTitle<T extends { title: string }>(Title: string, books: T[]) {
   const foundBook = books.find(book => book.title === Title);
@@ -42,10 +55,68 @@ function findItemByTitle<T extends { title: string }>(Title: string, books: T[])
   } else {
     console.log('Kein Treffer!')
     return null;
+  };
+};
+
+class Library {
+
+  addBook(book: Book): void{
+    books.push(book);
+  };
+
+  getAvailableBooks(){
+    if(books.length > 0){
+      books.forEach((book) => console.log(book.title));
+    } else {console.log('No books found!')};
+
+  };
+
+  borrowBook(isbn: string, user: User): void {
+      const foundBook = books.find(book => book.isbn === isbn);
+      if(foundBook){
+        if(foundBook?.available === true){
+          user.borrowedBooks.push(foundBook);
+          foundBook.available = false;
+          console.log(`${foundBook.title} wurde an ${user.name} ausgeliehen!`);
+        } else {
+          console.log(`${foundBook.title} ist nicht verfügbar!`)
+        }
+      } else {console.log('Buch wurde nicht gefunden!')}
+  };
+
+  returnBook(isbn: string, user: User): void {
+      const foundBook = books.find(book => book.isbn === isbn);
+      if(foundBook){
+          if(foundBook.available === false){
+            user.borrowedBooks = user.borrowedBooks.filter(foundBook => !foundBook);
+            foundBook.available = true;
+            console.log(`${foundBook.title} wurde von ${user.name} zurückgegeben!`);
+          } else{console.log(`${foundBook.title} wurde bereits zurückgegeben!`)};
+        } 
+       else {console.log('Buch wurde nicht gefunden!')}
+  };
+
+  getBooksByCategory(category: BookCategory,books: Book[]){
+    const foundBook = books.find(book => book.category === category);
+      if(foundBook){
+        console.log(`Treffer: ${foundBook.category}!`)
+            return foundBook;
+      } else {
+        console.log('Kein Treffer!')
+        return null;
   }
-  
-}
+  }
+};
 
 
-findItemByTitle('test', books);
-findItemByTitle('potter', books);
+const BookChanger = new Library;
+
+BookChanger.borrowBook('1945',Eddy);
+BookChanger.returnBook('1945',Eddy);
+BookChanger.getBooksByCategory(BookCategory.Fiction,books);
+BookChanger.returnBook('1945',Eddy);
+
+
+
+
+
