244f6bafe2dfe9fb6b906235a73bda6f845b67af
Dumary75 <gleisal.neu@proton.me>
2025-10-10 11:49:01 +0200

    Learned about middleware

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ const express = require('express');
 
 const start = express();
 
+start.use((req, res, next) => {
+ console.log('Hallo');
+ next();
+})
+
+start.use('/', (req, res, next) => {
+ res.send('<h1>Hallo</h1>')
+})
+
 const server = http.createServer(start);
 
 server.listen(3000);
