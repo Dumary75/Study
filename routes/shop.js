85ce21f50a98672bf5281fdbc45227a1117f2a67
Dumary75 <gleisal.neu@proton.me>
2025-10-22 12:19:54 +0200

    Work with squelize etc.

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,19 @@
 
 const express = require('express');
+const db = require('../util/database');
 const { products } = require('./admin');
 
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
- res.render('shop', {titleProb: 'ShopSeite' , products: products});
+    db.execute('INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
+        ['AndereTest', '9.79', 'bkaiskda', '']
+    )
+    db.execute('SELECT * FROM products').then(([rows]) => {
+        res.render('shop', {titleProb: 'ShopSeite' , products: rows});
+    }).catch((error) => {
+        console.log(error);
+    })
 })
 
 exports.routes = router;
