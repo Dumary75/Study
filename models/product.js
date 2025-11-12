4dc4a0efa3e026f6cdbd4c360c0aa1ddb8f0e68e
Dumary75 <gleisal.neu@proton.me>
2025-11-12 17:09:20 +0100

    Just work with mongoDB

--- models/product.js
+++ models/product.js
@@ -11,31 +11,14 @@ constructor(title,price,imageUrl,description){
 };
 
 save(){
-  
+  const db = getDb();
+  db.collection('product').insertOne(this).then((resault) =>
+    console.log(resault)
+  ).catch((err) => console.log(err));
 }
 
 }
 
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-});
+
 
 module.exports = Product;
