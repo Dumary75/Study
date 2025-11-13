4825e7f8fe43c6dcf89c089207f72c0373302d0c
Dumary75 <gleisal.neu@proton.me>
2025-11-13 11:11:57 +0100

    Learn more about mongoDB logic

--- models/product.js
+++ models/product.js
@@ -1,24 +1,51 @@
-
 const getDb = require('../util/database').getDb;
+const mongoDB = require('mongodb');
 
-class Product{
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
+  }
 
-constructor(title,price,imageUrl,description){
-  title = this.title,
-  price = this.price,
-  imageUrl = this.imageUrl,
-  description = this.description
-};
+  save() {
+    const db = getDb();
+    return db.collection('products')
+      .insertOne(this)
+      .then(result => {
+        console.log('Product inserted:', result);
+        return result;
+      })
+      .catch(err => console.error('Error inserting product:', err));
+  }
 
-save(){
-  const db = getDb();
-  db.collection('product').insertOne(this).then((resault) =>
-    console.log(resault)
-  ).catch((err) => console.log(err));
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products')
+      .find()
+      .toArray()
+      .then(result => {
+        console.log(result);
+        return result;
+      })
+      .catch(err => console.log(err));
+  }
+
+  static findById(productId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find({ _id: new mongoDB.ObjectId(productId) })
+      .next()
+      .then(result => {
+        console.log(result);
+        return result;
+      })
+      .catch(err => console.log(err));
+  }
+
+
 }
-
-}
-
-
 
 module.exports = Product;
