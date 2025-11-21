a5c95bf8920b9285f740cd24d74fd8dccb73a52e
Dumary75 <gleisal.neu@proton.me>
2025-11-21 14:52:16 +0100

    Working again on cookies / seasons

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,12 @@ exports.getLogin = (req, res, next) => {
 };
 
 
-exports.postLogin = (req, res, next) => {
+exports.postLogin = (req, res) => {
   req.session.isLoggedIn = true;
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+    req.session.user = user;
+  })
+  .catch(err => console.log(err));
   res.redirect('/');
 };
