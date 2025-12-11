ee6352288482dd539f03dc0dd11f6929fa420cc6
Dumary75 <gleisal.neu@proton.me>
2025-12-11 14:28:54 +0100

    Learn mongoose

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const path = require('path');
 
-const User = require('./models/user');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('693309c3d91a4bf1e29c4711')
     .then(user => {
-      req.user = new User( user.email, user.password, user.card, user.id );
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
@@ -33,6 +32,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    'mongodb+srv://firstOne:9lPVJUcMUK9yrURY@schwarzmuller.yuepgkj.mongodb.net/?appName=Schwarzmuller'
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
