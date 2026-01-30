78dcc55a56957ab25fec1c45201c9e4fb48996df
Dumary75 <gleisal.neu@proton.me>
2026-01-30 15:33:41 +0100

    Learn about API call in C#

--- WetterApp/MainWindow.axaml.cs
+++ WetterApp/MainWindow.axaml.cs
@@ -1,11 +1,26 @@
+using System.Net.Http;
 using Avalonia.Controls;
 
 namespace WetterApp;
 
 public partial class MainWindow : Window
 {
+
+        public readonly string  apiKey= "b5342b70c4e9dd024baafd6164f9f1ca";
+        private readonly string apiRequest = "https://api.openweathermap.org/data/2.5/weather";
+
     public MainWindow()
     {
         InitializeComponent();
+
+        HttpClient httpClient = new HttpClient();
+
+        var city = "Reutlingen";
+        var finalURL = apiRequest + "?q=" + city + "&appid=" + apiKey;
+
     }
+
+
+
+
 }
--- WetterApp/obj/Debug/net10.0/WetterApp.AssemblyInfo.cs
+++ WetterApp/obj/Debug/net10.0/WetterApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WetterApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6cf2dd32f54654048a0f58743ce19f27c9d2716")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2208e1e1fa7262cc1211fad8878697a066df6e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WetterApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WetterApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
