264f1e743e103d2c33cff90b4d365bf380a471fe
Dumary75 <gleisal.neu@proton.me>
2026-08-11 14:53:08 +0200

    Continue learning...

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532ccaf6223306ab7547682c6b9c9269b1b4bf97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00807d439ffab3165f2f999a72657af8758fb80c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
